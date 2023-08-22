8c813f58845b993448abdeac7d5c33cc44edd0b3
sistemasfsoc <140452716+sistemasfsoc@users.noreply.github.com>
2023-08-22 12:49:11 -0300

    Delete php curso directory

--- php curso/controladores/plantilla.controlador.php
+++
@@ -1,9 +0,0 @@
-<?php
-//controlador
-class ControladorPlantilla{
-    public function ctrGetPlantilla(){
-        include "vistas/plantillas.php";
-
-    }
-}
-?>
--- php curso/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-//requiero a plantilla.controlador.php
-require_once "controladores/plantilla.controlador.php";
-
-//instancio el objeto
-$plantilla = new ControladorPlantilla();
-
-//Ejecuto el metodo
-$plantilla  ->ctrGetPlantilla();
